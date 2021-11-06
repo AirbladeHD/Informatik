12c0aa931140858f326f0e4927ce2622217b11e9
AirbladeHD <tamanu0510@gmail.com>
2021-11-06 16:49:25 +0100

    Bruteforce added

--- bruteforce.py
+++ bruteforce.py
@@ -1,5 +1,28 @@
-import dukeHash
+from itertools import product
+import time
+from dukeHash import hash
 
-print(dukeHash.hash("Hello World"))
-print(dukeHash.hash("Hello World!"))
-print(dukeHash.hash("Hello World!areiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiöjsssssssssssssssssssssssssaoiewrrrrrrrrrrrrrrrrrrrrrrrrrrrrzfgoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewaoiewazgbssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss"))
+def bruteforce():
+
+    nonce = "jfdh"
+    chars = 'ABCDEFGHIJKLMOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz' # chars to look for
+    attempts = 0
+
+    t0 = time.time()
+
+    for length in range(1, len(nonce) + 1): # only do lengths of 1 + 2
+        to_attempt = product(chars, repeat=length)
+        for attempt in to_attempt:
+            if(hash(''.join(attempt)) == hash(nonce)):
+                t1 = time.time()
+                total = t1-t0
+                return ("Versuche: " + str(attempts) + " Nonce: " + ''.join(attempt) + " Zeit: " + str(total))
+            print(hash(''.join(attempt)))
+            attempts = attempts + 1
+
+    t1 = time.time()
+    total = t1-t0
+
+    print("Nonce nicht gefunden. Versuche: " + str(attempts))
+
+print(bruteforce())
