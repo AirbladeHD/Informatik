fd3ed968b2921e877ab880865e238bae6d05f2a7
AirbladeHD <tamanu0510@gmail.com>
2021-11-09 15:04:28 +0100

    General Update

--- bruteforce.py
+++ bruteforce.py
@@ -4,7 +4,8 @@ from dukeHash import hash
 
 def bruteforce():
 
-    nonce = "jfdh"
+    nonce = "jdfh"
+    nonceHash = "duke$3e93ea3ea3e93ea3ea3e93ea3ea3eb3ec3eb3eb3e93ea3ed"
     chars = 'ABCDEFGHIJKLMOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz' # chars to look for
     attempts = 0
 
@@ -13,7 +14,7 @@ def bruteforce():
     for length in range(1, len(nonce) + 1): # only do lengths of 1 + 2
         to_attempt = product(chars, repeat=length)
         for attempt in to_attempt:
-            if(hash(''.join(attempt)) == hash(nonce)):
+            if(hash(''.join(attempt)) == nonceHash):
                 t1 = time.time()
                 total = t1-t0
                 return ("Versuche: " + str(attempts) + " Nonce: " + ''.join(attempt) + " Zeit: " + str(total))
