b7b83393e33e04ef2c2e808a7aad236dc33b3a12
AirbladeHD <tamanu0510@gmail.com>
2021-11-09 15:27:02 +0100

    Freie Tage Update

--- gehaltsrechner.py
+++ gehaltsrechner.py
@@ -1,3 +1,21 @@
+import math
+
+def ist_schaltjahr(jahr):
+    if(jahr%400 == 0 or jahr%4 == 0 and jahr%100 != 0):
+        return True
+    else:
+        return False
+
+def anzahl(jahr, monat, returnNames):
+    months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    names = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"]
+    if(ist_schaltjahr(jahr)):
+        months[1] = 29
+    if(returnNames):
+        return (months[monat - 1], names[monat - 1], jahr)
+    else:
+        return months[monat - 1]
+
 def existenz(jahr, monat, tag):
    if(tag > anzahl(jahr, monat, False) or tag < 1):
        return False
@@ -19,38 +37,31 @@ def wochentag(jahr, monat, tag):
     d = tag + k + j + int(j/4) - 2 * (c%4)
     e = d%7
     return tage[e]
-        
 
-def ist_schaltjahr(jahr):
-    if(jahr%400 == 0 or jahr%4 == 0 and jahr%100 != 0):
-        return True
-    else:
-        return False
-        
-def anzahl(jahr, monat, returnNames):
-    months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    names = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"]
-    if(ist_schaltjahr(jahr)):
-        months[1] = 29
-    s = wochentag(2021, 10, 3)
-    #i = 2
-    #while(i < int(months[monat - 1])):
-    #if(wochentag(jahr, monat, i) == "Sonntag"):
-        #s = s + 1
-        #i = i + 1
-    if(returnNames):
-        return (months[monat - 1], names[monat - 1], jahr, s)
-    else:
-        return months[monat - 1]
+def freieTage(jahr, monat, weekend):
+    tage = int(anzahl(jahr, monat, False))
+    i = 1
+    s = 0
+    while(i < tage):
+        if(weekend):
+            if(wochentag(jahr, monat, i) == "Sonntag" or wochentag(jahr, monat, i) == "Samstag"):
+                s = s + 1
+        else:
+            if(wochentag(jahr, monat, i) == "Sonntag"):
+                s = s + 1
+        i = i + 1
+    return s
 
-#stundenlohn = float(input("Stundenlohn: "))
-#tageslohn = stundenlohn * int(input("Tagesstundenzahl: "))
-var = anzahl(int(input("Jahr: ")), int(input("Monat: ")), True)
-#steuer = int(input("Steuersatz in Prozent: "))
+stundenlohn = float(input("Stundenlohn: "))
+tageslohn = stundenlohn * int(input("Tagesstundenzahl: "))
+jahr = int(input("Jahr: "))
+monat = int(input("Monat: "))
+samstag = False
+if(input("Samstag freier Tag? Ja/Nein: ") == "Ja"):
+    samstag = True
+var = anzahl(jahr, monat, True)
+steuer = int(input("Steuersatz in Prozent: "))
 
-#print("Bruttomonatslohn für " + str(var[1]) + " " + str(var[2]) + ": " + str(var[0] * tageslohn) + "€")
+print("Bruttomonatslohn für " + str(var[1]) + " " + str(var[2]) + " ≈ " + str(math.ceil((var[0] - freieTage(jahr, monat, samstag)) * tageslohn)) + "€")
 
-#print("Nettomonatslohn für " + str(var[1]) + " " + str(var[2]) + ": " + str(var[0] * tageslohn - (var[0] * tageslohn / 100 * steuer)) + "€")
-print("Sonntage: " + str(var[3]))
-#print(anzahl(2021, 2)[3])
-#print(wochentag(2021, 2, 1))
+print("Nettomonatslohn für " + str(var[1]) + " " + str(var[2]) + " ≈ " + str(math.ceil((var[0] - freieTage(jahr, monat, samstag)) * tageslohn - (var[0] * tageslohn / 100 * steuer))) + "€")
